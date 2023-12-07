c7b25933a2d17fd231003acf8ac7116821d187f0
felri <felipercmelo@gmail.com>
2023-12-07 12:56:45 -0300

    multiple stores step 1

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,13 @@
 import { create } from "zustand";
 import { addEdge, applyNodeChanges, applyEdgeChanges } from "reactflow";
 import { persist } from "zustand/middleware";
-import { createInputBelowOutputNode, deleteOneNode } from "./utils";
+import {
+  createInputBelowOutputNode,
+  deleteOneNode,
+  getMessageHistory,
+  createNewOutputNode,
+  defaultEdgeOptions,
+} from "./utils";
 import OpenAI from "openai";
 
 const initialNodes = [
@@ -29,15 +35,13 @@ const initialEdges = [
     id: "e1-2",
     source: "1",
     target: "2",
-    animated: false,
-    type: "smoothstep",
+    ...defaultEdgeOptions,
   },
   {
     id: "e2-3",
     source: "2",
     target: "3",
-    animated: false,
-    type: "smoothstep",
+    ...defaultEdgeOptions,
   },
 ];
 
@@ -46,96 +50,202 @@ const initialLayouted = {
   edges: initialEdges,
 };
 
-function getMessageHistory(id, systemMessage, nodes, edges) {
-  console.log("getMessageHistory", nodes, edges);
-  const history = [];
+// Factory function to create a new store
+const createStore = (initialLayouted, id) =>
+  create(
+    persist(
+      (set, get) => ({
+        id, // unique identifier for each store
+        nodes: initialLayouted.nodes,
+        edges: initialLayouted.edges,
+        openAIInstance: null,
+        openAIConfig: {
+          apiKey: "",
+          engine: "gpt-3.5-turbo-1106",
+          temperature: 0.9,
+          systemMessage: "You are a chatbot. You are talking to a human.",
+        },
+        updateOpenAIConfig: (newConfig) => {
+          set((state) => ({
+            openAIConfig: { ...state.openAIConfig, ...newConfig },
+          }));
+        },
+        updateOpenAIKey: (newKey) => {
+          set((state) => ({
+            openAIConfig: { ...state.openAIConfig, ...newKey },
+            openAIInstance: new OpenAI({
+              apiKey: newKey.apiKey,
+              dangerouslyAllowBrowser: true,
+            }),
+          }));
+        },
+        createOpenAIInstance: () => {
+          set((state) => ({
+            openAIInstance: new OpenAI({
+              apiKey: state.openAIConfig.apiKey,
+              dangerouslyAllowBrowser: true,
+            }),
+          }));
+        },
+        onNodesChange: (changes) => {
+          set({
+            nodes: applyNodeChanges(changes, get().nodes),
+          });
+        },
+        onEdgesChange: (changes) => {
+          set({
+            edges: applyEdgeChanges(changes, get().edges),
+          });
+        },
+        onConnect: (connection) => {
+          set({
+            edges: addEdge(connection, get().edges),
+          });
+        },
+        onUpdateUserInput: (text, id) => {
+          set((state) => ({
+            nodes: state.nodes.map((node) => {
+              if (node.id === id) {
+                return { ...node, data: { text } };
+              }
+              return node;
+            }),
+          }));
+        },
+        deleteUserNode: (id) => {
+          const layouted = deleteOneNode(get().nodes, get().edges, id);
+          set({
+            nodes: layouted.nodes,
+            edges: layouted.edges,
+          });
+        },
+        deleteChatNode: (deletedNodes, nodes, edges) => {
+          deletedNodes.forEach((node) => {
+            const layouted = deleteOneNode(nodes, edges, node.id);
+            set({
+              nodes: layouted.nodes,
+              edges: layouted.edges,
+            });
+          });
+        },
+        onUpdateUserQuantity: (quantity, id) => {
+          set((state) => ({
+            nodes: state.nodes.map((node) => {
+              if (node.id === id) {
+                return { ...node, data: { quantity } };
+              }
+              return node;
+            }),
+          }));
+        },
+        getHistory: (id) => {
+          const sytemMessage = get().openAIConfig.systemMessage;
+          return getMessageHistory(id, sytemMessage, get().nodes, get().edges);
+        },
+        onUserInputSend: (id, parentHeight) => {
+          // create a new output node and edge
+          const layouted = createNewOutputNode(
+            get().nodes,
+            get().edges,
+            id,
+            parentHeight
+          );
 
-  let currentNodeId = id;
-  while (currentNodeId !== null) {
-    const currentNode = nodes.find((node) => node.id === currentNodeId);
-    if (currentNode && currentNode.data?.text) {
-      // Prepend system messages and user input to the history array.
-      history.unshift({
-        role: currentNode.type === "userInput" ? "user" : "assistant",
-        content: currentNode.data.text,
-      });
-    }
-    // Find the edge that points to the current node.
-    const currentEdge = edges.find((edge) => edge.target === currentNodeId);
-    // Update the currentNodeId to be the source of the current edge (to move to the previous node).
-    currentNodeId = currentEdge ? currentEdge.source : null;
-  }
+          const lastNode = layouted.nodes[layouted.nodes.length - 1];
 
-  // Prepend the initial system message to the history array.
-  history.unshift({
-    role: "assistant",
-    content: systemMessage,
-  });
+          // create a new input node and edge
+          const inputLayouted = createInputBelowOutputNode(
+            layouted.nodes,
+            layouted.edges,
+            lastNode.id
+          );
 
-  return history;
-}
+          set({
+            nodes: inputLayouted.nodes,
+            edges: inputLayouted.edges,
+          });
+        },
+        createNewInputNode: (id, parentHeigth) => {
+          const layouted = createInputBelowOutputNode(
+            get().nodes,
+            get().edges,
+            id,
+            parentHeigth
+          );
 
-function calculateNewNodePosition(nodes, edges, parentId, parentHeight) {
-  const horizontalOffset = 800; // Horizontal offset for each child
-  const verticalSpacing = 150; // Vertical spacing from the parent node
-
-  // Find the parent node
-  const parentNode = nodes.find((node) => node.id === parentId);
-  if (!parentNode) {
-    throw new Error("Parent node not found");
-  }
-
-  // Count the number of child nodes for the given parent
-  const childCount = edges.filter((edge) => edge.source === parentId).length;
-
-  // Calculate the x position based on the number of child nodes
-  const newXPosition = childCount * horizontalOffset + parentNode.position.x;
-
-  // Calculate the y position based on the parent node's position and height
-  // Add some additional spacing to avoid overlapping with the parent node
-  const newYPosition = parentNode.position.y + parentHeight + verticalSpacing;
-
-  return {
-    x: newXPosition,
-    y: newYPosition,
-  };
-}
-
-function createNewOutputNode(
-  nodes,
-  edges,
-  parentId,
-  parentHeight,
-  type = "chatOutput"
-) {
-  const newNodePosition = calculateNewNodePosition(
-    nodes,
-    edges,
-    parentId,
-    parentHeight
+          set({
+            nodes: layouted.nodes,
+            edges: layouted.edges,
+          });
+        },
+        updateChildrenPosition: (id, diff) => {
+          const layouted = get().nodes.map((node) => {
+            if (node.id === id) {
+              return {
+                ...node,
+                position: {
+                  x: node.position.x,
+                  y: node.position.y + diff,
+                },
+              };
+            }
+            return node;
+          });
+          set({ nodes: layouted });
+        },
+      }),
+      {
+        name: `store-${id}`, // unique key for local storage persistence
+        partialize: (state) =>
+          Object.fromEntries(
+            Object.entries(state).filter(([key]) =>
+              ["openAIConfig", "id", "nodes", "edges"].includes(key)
+            )
+          ),
+      }
+    )
   );
 
-  const newNodeId = `${parseInt(nodes[nodes.length - 1].id) + 1}`;
-  const newNode = {
-    id: newNodeId,
-    type,
-    data: { text: "", id: newNodeId },
-    position: newNodePosition,
-  };
+const storeManager = {
+  stores: {},
 
-  const newEdge = {
-    id: `e${parentId}-${newNodeId}`,
-    source: parentId,
-    target: newNodeId,
-    animated: false,
-    type: "smoothstep",
-  };
+  initializeStores() {
+    const storeIds = this.getStoredStoreIds();
+    storeIds.forEach((id) => {
+      this.stores[id] = createStore({}, id);
+    });
+  },
 
-  return {
-    nodes: [...nodes, newNode],
-    edges: [...edges, newEdge],
-  };
-}
+  createNewStore(initialLayouted, id) {
+    const newStore = createStore(initialLayouted, id);
+    this.stores[id] = newStore;
+    return newStore;
+  },
+
+  getStore(id) {
+    return this.stores[id];
+  },
+
+  removeStore(id) {
+    delete this.stores[id];
+    localStorage.removeItem(`store-${id}`); // Remove from local storage
+  },
+
+  getStoredStoreIds() {
+    const storeIds = [];
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i);
+      if (key && key.startsWith("store-")) {
+        const id = key.substring(6); // Extract ID part of the key
+        storeIds.push(id);
+      }
+    }
+    return storeIds;
+  },
+};
+
+// Initialize store manager when app starts
+storeManager.initializeStores();
 
 const useStore = create(
   persist(
--- src/store/utils.js
+++ src/store/utils.js
@@ -1,3 +1,8 @@
+export const defaultEdgeOptions = {
+  animated: false,
+  type: "smoothstep",
+};
+
 export function deleteOneNode(nodes, edges, nodeId) {
   // Find the parent of the node
   const parentEdge = edges.find((edge) => edge.target === nodeId);
@@ -29,8 +34,7 @@ export function deleteOneNode(nodes, edges, nodeId) {
           id: `e${parentId}-${childId}`,
           source: parentId,
           target: childId,
-          animated: false,
-          type: "smoothstep",
+          ...defaultEdgeOptions,
         });
       });
     }
@@ -68,8 +72,7 @@ export function createInputBelowOutputNode(nodes, edges, parentId, parentHeight)
     id: `e${parentId}-${newNodeId}`,
     source: parentId,
     target: newNodeId,
-    animated: false,
-    type: "smoothstep",
+    ...defaultEdgeOptions
   };
 
   return {
@@ -77,3 +80,95 @@ export function createInputBelowOutputNode(nodes, edges, parentId, parentHeight)
     edges: [...edges, newEdge],
   };
 }
+
+
+export function getMessageHistory(id, systemMessage, nodes, edges) {
+  console.log("getMessageHistory", nodes, edges);
+  const history = [];
+
+  let currentNodeId = id;
+  while (currentNodeId !== null) {
+    const currentNode = nodes.find((node) => node.id === currentNodeId);
+    if (currentNode && currentNode.data?.text) {
+      // Prepend system messages and user input to the history array.
+      history.unshift({
+        role: currentNode.type === "userInput" ? "user" : "assistant",
+        content: currentNode.data.text,
+      });
+    }
+    // Find the edge that points to the current node.
+    const currentEdge = edges.find((edge) => edge.target === currentNodeId);
+    // Update the currentNodeId to be the source of the current edge (to move to the previous node).
+    currentNodeId = currentEdge ? currentEdge.source : null;
+  }
+
+  // Prepend the initial system message to the history array.
+  history.unshift({
+    role: "assistant",
+    content: systemMessage,
+  });
+
+  return history;
+}
+
+export function calculateNewNodePosition(nodes, edges, parentId, parentHeight) {
+  const horizontalOffset = 800; // Horizontal offset for each child
+  const verticalSpacing = 150; // Vertical spacing from the parent node
+
+  // Find the parent node
+  const parentNode = nodes.find((node) => node.id === parentId);
+  if (!parentNode) {
+    throw new Error("Parent node not found");
+  }
+
+  // Count the number of child nodes for the given parent
+  const childCount = edges.filter((edge) => edge.source === parentId).length;
+
+  // Calculate the x position based on the number of child nodes
+  const newXPosition = childCount * horizontalOffset + parentNode.position.x;
+
+  // Calculate the y position based on the parent node's position and height
+  // Add some additional spacing to avoid overlapping with the parent node
+  const newYPosition = parentNode.position.y + parentHeight + verticalSpacing;
+
+  return {
+    x: newXPosition,
+    y: newYPosition,
+  };
+}
+
+export function createNewOutputNode(
+  nodes,
+  edges,
+  parentId,
+  parentHeight,
+  type = "chatOutput"
+) {
+  const newNodePosition = calculateNewNodePosition(
+    nodes,
+    edges,
+    parentId,
+    parentHeight
+  );
+
+  const newNodeId = `${parseInt(nodes[nodes.length - 1].id) + 1}`;
+  const newNode = {
+    id: newNodeId,
+    type,
+    data: { text: "", id: newNodeId },
+    position: newNodePosition,
+  };
+
+  const newEdge = {
+    id: `e${parentId}-${newNodeId}`,
+    source: parentId,
+    target: newNodeId,
+    ...defaultEdgeOptions
+  };
+
+  return {
+    nodes: [...nodes, newNode],
+    edges: [...edges, newEdge],
+  };
+}
+
